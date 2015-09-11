a6435780468bd72ffdd3be307fa875aa8c7ef8e8
zhpn1024 <zhpn1024@163.com>
2015-09-11 21:19:00 +0000

    add BH

--- exp.py
+++ exp.py
@@ -1,10 +1,10 @@
 class exp():
-  def __init__(self, sample, exp): #sample and expression list
+  def __init__(self, sample, data): #sample and expression list
     self.sample = sample
-    self.exp = exp
+    self.data = data
     self.value = [] #sort index
   def __str__(self):
-    return '\t'.join(map(str, self.exp))
+    return '\t'.join(map(str, self.data))
   def __repr__(self):
     return self.headerline + "\n" + str(self)
   def __len__(self):
@@ -20,9 +20,9 @@ class exp():
     return sep.join(map(str, self.sample))
     
 class trans(exp):
-  def __init__(self, tid, sample, exp):
+  def __init__(self, tid, sample, data):
     self.id = tid
-    exp.__init__(self, sample, exp)
+    exp.__init__(self, sample, data)
   def __str__(self):
     return self.id + '\t' + exp.__str__(self)
   def __repr__(self):
@@ -35,9 +35,9 @@ class trans(exp):
     return 'tid' + sep + sep.join(map(str, self.sample))
     
 class gene(exp):
-  def __init__(self, gid, sample, exp):
+  def __init__(self, gid, sample, data):
     self.id = gid
-    exp.__init__(self, sample, exp)
+    exp.__init__(self, sample, data)
     self.trans = []
   def __str__(self):
     if len(self.trans) == 0:
@@ -82,15 +82,15 @@ def gtexp_iter(expfile, gi = 0, ti = 1, sep = '\t', ei = [], sample = []):
     n = len(lst)
     gid = lst[gi]
     tid = lst[ti]
-    exp = map(float, subarr(lst, ei, ti+1))
-    t = Trans(tid, sample, exp)
+    data = map(float, subarr(lst, ei, ti+1))
+    t = Trans(tid, sample, data)
     t.gid = gid
     yield t
 def gtexp_load(expfile, gi = 0, ti = 1, sep = '\t', ei = [], sample = []):
   gs = {}
   for t in gtexp_iter(expfile, gi, ti, sep, ei, sample):
     if t.gid not in gs:
-      gs[t.gid] = gene(t.gid, t.sample, t.exp)
+      gs[t.gid] = gene(t.gid, t.sample, t.data)
     gs[t.gid].add_trans(t)
   return gs
 
@@ -104,8 +104,8 @@ def texp_iter(expfile, ti = 0, sep = '\t', ei = [], sample = []):
     n = len(lst)
     #gid = lst[gi]
     tid = lst[ti]
-    exp = map(float, subarr(lst, ei, ti+1))
-    t = Trans(tid, sample, exp)
+    data = map(float, subarr(lst, ei, ti+1))
+    t = Trans(tid, sample, data)
     #t.gid = gid
     yield t
 
@@ -119,8 +119,30 @@ def gexp_iter(expfile, gi = 0, sep = '\t', ei = [], sample = []):
     n = len(lst)
     #gid = lst[gi]
     gid = lst[gi]
-    exp = map(float, subarr(lst, ei, gi+1))
-    g = gene(gid, sample, exp)
+    data = map(float, subarr(lst, ei, gi+1))
+    g = gene(gid, sample, data)
     #t.gid = gid
     yield g
 
+class profile():
+  def __init__(self):
+    self.exps = {}
+  def add_exp(self, exp):
+    self.exps[exp.id] = exp
+  def __len__(self):
+    return len(self.exps)
+  def BHcorrection(self, pid = -1, total = -1):
+    lst = self.exps.values()
+    n = len(lst)
+    if total < 0: total = n
+    for e in lst:
+      if len(e.value) < 1: e.value.append(1)
+      e.value[0] = e.data[pid]
+    lst.sort()
+    qc = 1
+    for i in range(n-1, -1, -1):
+      q = float(lst[i].value[0]) * total / (i+1)
+      if q > qc : q = qc
+      lst[i].q = q
+      qc = q
+    return lst
